065a35d5da8d2cd10680c01d97c427a5e230f31b
Nasrallah <nsrallahh333@gmail.com>
2024-07-07 10:20:55 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Course</title>
+</head>
+
+<body>
+    <h1> <?php print ("The Php Cousre"); ?></h1>
+</body>
+
+</html>
