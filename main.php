dc5b4ad42f4563dad0b48bde9531608c5d0af4e2
Nasrallah <nsrallahh333@gmail.com>
2024-10-21 11:23:14 +0300

    add new php file

---
+++ main.php
@@ -0,0 +1,22 @@
+<?php
+$title = "Abo Omar";
+$description = "Abo Omar is a software engineer and a web developer";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?php echo $title ?></title>
+</head>
+
+<body>
+    <h1><?php echo "Welcom to" . $title ?> </h1>
+    <?php echo "<br>" . $description ?>
+
+
+</body>
+
+</html>
